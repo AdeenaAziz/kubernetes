8c70c9c6d641574dc8703a5ee21a748fa0ad23a5
adeena <seemakhan91155@gmail.com>
2026-02-01 16:02:12 +0530

    version 3: added type validation

--- addition.py
+++ addition.py
@@ -1,6 +1,7 @@
 def add(a, b):
-    if a < 0 or b < 0:
-        return "Negative numbers not allowed"
-    return a + b
+        if not isinstance(a, int) or not isinstance(b, int):
+             return "Only integers allowed"
+        return a + b
+
 print(add(2, 3))
 
